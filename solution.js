ed053187580df6da7ebb56b391ca0a2c660d87e6
Chuludcjr <51537607+Chuludcjr@users.noreply.github.com>
2019-06-14 14:26:55 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,2 +1,19 @@
 // your solution should go here
 // for demo purposes use the console for any out you will need.
+// Solutions to 1
+ var recipe = {
+    'title': 'Cooking Beef',
+    'servings': 2,
+    'ingredients': ['Beef','Cooking oil ', 'Tomato ', 'Onion']
+};
+
+console.log(recipe.title);
+console.log('Servings: ' + recipe.servings);
+console.log('Ingredients:');
+
+//The loop outputs all the items in the array
+for(let todo of recipe.ingredients){
+    console.log(todo);
+}
+
+//solutions to 2
