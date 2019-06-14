82f470bc1ed83d856eeed3b902ee04e829581b47
Chuludcjr <51537607+Chuludcjr@users.noreply.github.com>
2019-06-14 14:43:45 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -17,3 +17,25 @@ for(let todo of recipe.ingredients){
 }
 
 //solutions to 2
+var books = [
+    {title: 'Ticklish Sansation',
+     author: 'Gideon Phiri',
+     //variable book status to store boolean
+     bookStatus: true
+    },
+    {title: 'Imitation is limitation',
+    author: 'John Mason',
+    //variable book status to store boolean
+    bookStatus: false
+    }];
+    //loop to output the elements in the array
+  for (var i = 0; i < books.length; i++) {
+    var book = books[i];
+    var bookInfo = book.title + ' by ' + book.author;
+    //if & else to check if book status is true or false to give output
+    if (book.bookStatus) {
+      console.log('You already read ' + bookInfo);
+    } else {
+      console.log('You still need to read ' + bookInfo);
+    }
+  }
